f2dba192db1eafb81e9e090ca2594c0303632d93
Pratik <sutharpratik10@hotmail.com>
2024-01-10 22:48:59 +0530

    login, registration server actions

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -5,7 +5,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { z } from "zod"
 import { useState, useTransition } from "react"
 
-import { LoginSchema } from "@/schemas"
+import { RegisterSchema } from "@/schemas"
 import { Input } from "@/components/ui/input"
 import {
     Form,
@@ -19,29 +19,30 @@ import { CardWrapper } from "@/components/auth/card-wrapper"
 import { Button } from "@/components/ui/button"
 import { FormError } from "@/components/form-error"
 import { FormSuccess } from "@/components/form-success"
-import { login } from "@/actions/login"
+import { register } from "@/actions/register"
 import { error } from "console"
 
-export const LoginForm = () => {
+export const RegisterForm = () => {
     const [error, setError] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string | undefined>("");
 
     const [isPending, startTransition] = useTransition();
 
-    const form = useForm<z.infer<typeof LoginSchema>>({ 
-        resolver: zodResolver(LoginSchema),
+    const form = useForm<z.infer<typeof RegisterSchema>>({ 
+        resolver: zodResolver(RegisterSchema),
         defaultValues:{
+            name:"",
             email:"",
             password:"",
         }
     });
 
-    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
         setError("");
         setSuccess("");
 
         startTransition(()=>{
-            login(values)
+            register(values)
             .then ((data) => {
                 setError(data.error);
                 setSuccess(data.success);
@@ -52,14 +53,33 @@ export const LoginForm = () => {
     return (
         <>
             <CardWrapper 
-            headerLable="Welcome back"
-            backButtonLabel="Don't have an account?"
-            backButtonHref="/auth/register"
+            headerLable="create an account"
+            backButtonLabel="already have an account?"
+            backButtonHref="/auth/login"
             showSocial>
                 <Form {...form}>
                     <form onSubmit = {form.handleSubmit(onSubmit)}
                     className = "space-y-6">
                         <div className = "space-y-6">
+                        <FormField
+                                control={form.control} 
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input 
+                                                {...field}
+                                                disabled={isPending}
+                                                placeholder="John Doe"
+                                                type="Name"
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+
                             <FormField
                                 control={form.control} 
                                 name="email"
@@ -100,7 +120,7 @@ export const LoginForm = () => {
                             <FormSuccess message={success}/>
                             <Button
                              disabled={isPending} type="submit" className="w-full">
-                                Login
+                                Create an account
                             </Button>
                         </div>
                     </form>
--- schemas/index.ts
+++ schemas/index.ts
@@ -6,3 +6,13 @@ export const LoginSchema = z.object({
         message:"Password is required"
     })
 });
+
+export const RegisterSchema = z.object({
+    name: z.string().min(1,{
+        message:"name is required"
+    }),
+    email: z.string().email({message:"Email is required"}),
+    password: z.string().min(6,{
+        message:"Minimum 6 character password is required"
+    })
+});
